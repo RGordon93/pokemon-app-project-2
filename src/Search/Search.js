3ae71c96faa56bc882e612a636ef564895ab0e62
RGordon93 <contactrashawngordon@gmail.com>
2020-10-28 00:01:47 -0400

    added state to search

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Results from '../Results/Results'
 
 const Search = () => {
-const handleSubmit = (event) => {
-	event.preventDefault()
-}
+	const initialState = {
+		searchBar: '',
+	};
+	const [formState, setFormState] = useState(initialState);
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(formState);
+		setFormState(initialState);
+	};
+	const handleChange = (event) => {
+		setFormState({ ...formState, [event.target.id]: event.target.value });
+	};
 	return (
 		<div>
 			Search
-		<form onSubmit={handleSubmit}>
-			<label htmlFor='searchBar'>
-			   <input id='searchBar' />
-			   <button type='submit'>Submit</button>
-    
-			</label>
-		</form>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='searchBar'>
+					<input
+						id='searchBar'
+						onChange={handleChange}
+						value={formState.searchBar}
+					/>
+					<button type='submit'>Submit</button>
+				</label>
+			</form>
 		</div>
 	);
 };
-
 export default Search;
